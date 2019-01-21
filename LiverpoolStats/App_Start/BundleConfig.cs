63423e001292606c3eea1d70f19e8c2ec9568af1
Shyhiem Gilbert <shyhiemg@icloud.com>
2019-01-21 23:36:15 +0000

    Trying to change Bootstrap Theme and Nav-bar Layout (Not Working Yet)

--- LiverpoolStats/App_Start/BundleConfig.cs
+++ LiverpoolStats/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace LiverpoolStats
 					  "~/Scripts/respond.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
+					  "~/Content/bootstrap-simplex.css",
 					  "~/Content/site.css"));
 		}
 	}
